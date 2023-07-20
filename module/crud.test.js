0e0f1aef2b0ca24014e6c882f69337401fb2d6c5
mershark <mersharktakyi@gmail.com>
2023-07-20 08:48:08 +0000

    Update crud.test.js with test

--- module/crud.test.js
+++ module/crud.test.js
@@ -16,7 +16,16 @@ describe('add and remove items', () => {
     expect(tasks).toHaveLength(1)
   })
 
-  
-
-  
+  test ('delete an item from the list', () => {
+    
+    document.body.innerHTML = `
+    <div class='task-container'>
+      <div class='task'>
+      </div>
+    </div>
+    `
+    removeTask(localStorage)
+    const tasks = document.querySelectorAll('.task-container .task')
+    expect(tasks).toHaveLength(0)
+  })
 })
